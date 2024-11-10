3f6f483ef76cd1585bc0f07f07ac2bf133d7092c
Aftab35520 <161050400+Aftab35520@users.noreply.github.com>
2024-11-10 20:44:46 +0530

    first commit

--- app/Components/HomeComponent/HomeTocontact.js
+++ app/Components/HomeComponent/HomeTocontact.js
@@ -4,7 +4,7 @@ export default function HomeTocontact() {
   return (
     <div className="w-full  bg-[#1E1B1B] flex justify-center  ">
         <div className="w-[calc(100%-80px)] max-w-[1500px] pt-32">
-            <p className="text-white text-6xl font-medium max-w-[1000px] text-revel-white bigtext">Like Our Style? Let's Make an <span className=" text-revel-red font-bold">IMPACT</span> Together</p>
+            <p className="text-white text-6xl font-medium max-w-[1000px] text-revel-white bigtext ">Like Our Style? Let's Make an <span className=" text-revel-red ">IMPACT</span> ToGether</p>
             <p className="text-white max-w-[500px] mt-11 mb-11 smalltext">If you're working on something exceptional and believe it has the potential to make a real difference, we want to hear about it.</p>
             <div className="mb-32"><Link href={"/contact"} className=" bg-blue-50 p-4 rounded-3xl">Get in touch <span className="text-[16px] p-1 rounded-[50%]">➨</span> </Link></div>
         </div>
